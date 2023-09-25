a380e6e97fbd5d1194f3bc372b17492629ebfaf5
Aayush Rijal <aayush.rijal99@gmail.com>
2023-09-25 22:20:35 +1000

    Final test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,7 @@
+import Link from "next/link";
+import Image from "next/image";
+import logo from "@/images/logo/logo.png";
+
 function Home() {
   return (
     <main className="py-5">
@@ -6,7 +10,20 @@ function Home() {
           Hi, my name is Aayush Rijal!
         </h1>
         <p>I am a front-end developer with over 5 years of web development experience. Using JavaScript, PHP, and both relational and non-relational databases I craft robust and scalable digital solutions that offer tangible value to customers.</p>
+
       </div>
+        <nav className="border-b">
+            <div className="container py-4">
+                <div className="flex justify-between items-center">
+                    <div className="w-3/12">
+                        <Image src={logo.src} alt="logo" width={logo.width} height={logo.height} />
+                    </div>
+                    <div className='w-fit'>
+                        <Link href="/about">About</Link>
+                    </div>
+                </div>
+            </div>
+        </nav>
     </main>
   )
 }
