3e1a87752fc49d3182261a078c55a44e0e283bf4
Aayush Rijal <aayush.rijal99@gmail.com>
2023-09-25 22:10:33 +1000

    Final deployment test

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,13 +5,13 @@ import logo from "@/images/logo/logo.png";
 function Header() {
     return (
         <nav className="border-b">
-            <div className="container py-3">
+            <div className="container py-4">
                 <div className="flex justify-between items-center">
                     <div className="w-3/12">
                         <Image src={logo.src} alt="logo" width={logo.width} height={logo.height} />
                     </div>
                     <div className='w-fit'>
-                        <Link href="/about">About</Link>
+                        <Link href="/about">sAbout</Link>
                     </div>
                 </div>
             </div>
