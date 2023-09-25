0acc361b73b84fa02e5bc30622e989db63423fc6
Aayush Rijal <aayush.rijal99@gmail.com>
2023-09-25 21:29:32 +1000

    Project initialization

---
+++ src/Components/Header.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import Image from "next/image";
+import logo from "@/images/logo/logo.png";
+
+function Header() {
+    return (
+        <nav className="border-b">
+            <div className="container py-3">
+                <div className="flex justify-between items-center">
+                    <div className="w-3/12">
+                        <Image src={logo.src} alt="logo" width={logo.width} height={logo.height} />
+                    </div>
+                    <div className='w-fit'>
+                        <Link href="/about">Projects</Link>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Header;
