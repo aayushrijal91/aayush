e8f8df9ff3963bdcba8a23adf21533f85b712fb7
Aayush Rijal <aayush.rijal99@gmail.com>
2023-09-25 21:34:03 +1000

    testing vercel deployment

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ function Home() {
     <main className="py-5">
       <div className="container">
         <h1 className="text-3xl">
-          Hi, my name is Aayush!
+          Hi, my name is Aayush Rijal!
         </h1>
         <p>I'm a front-end developer with 5+ years of web development experience. Using JavaScript, PHP, and both relational and non-relational databases I craft robust and scalable digital solutions that offer tangible value to customers.</p>
       </div>
