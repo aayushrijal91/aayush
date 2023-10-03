be1679d03cc3314ef61b96d3dffcde814587d103
Aayush Rijal <aayush.rijal99@gmail.com>
2023-10-03 22:31:13 +1100

    Banner added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,12 @@
 import Link from "next/link";
 import Image from "next/image";
 import logo from "@/images/logo/logo.png";
+import { Space_Mono } from 'next/font/google'
+
+const space = Space_Mono({
+  subsets: ['latin'],
+  weight: ['400', '700']
+});
 
 function Header() {
     return (
@@ -11,7 +17,7 @@ function Header() {
                         <Image src={logo.src} alt="logo" width={logo.width} height={logo.height} />
                     </div>
                     <div className='w-fit'>
-                        <Link href="/about">About</Link>
+                        <Link href="/about" className={`border-solid border-2 border-indigo-600 py-2 px-4 rounded-sm ${space.className}`}>Resume</Link>
                     </div>
                 </div>
             </div>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,17 @@
 import Header from '@/components/Header'
 import '@/styles/globals.css'
+import { Noto_Sans } from 'next/font/google'
+
+const noto = Noto_Sans({
+  subsets: ['latin'],
+  weight: ['100', '300', '400', '500', '700', '900']
+})
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <main className={noto.className}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </main>
   );
 }
