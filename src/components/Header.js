9c0668db33d4a21895f801d7aa52ed5a0eb7d9ae
Aayush Rijal <aayush.rijal99@gmail.com>
2023-10-03 22:44:49 +1100

    WIP pdf file

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import logo from "@/images/logo/logo.png";
 import { Space_Mono } from 'next/font/google'
+// import pdf from "../files/Aayush-Rijal-Resume.pdf";
 
 const space = Space_Mono({
   subsets: ['latin'],
@@ -17,7 +18,8 @@ function Header() {
                         <Image src={logo.src} alt="logo" width={logo.width} height={logo.height} />
                     </div>
                     <div className='w-fit'>
-                        <Link href="/about" className={`border-solid border-2 border-indigo-600 py-2 px-4 rounded-sm ${space.className}`}>Resume</Link>
+                        <a href="../files/Aayush-Rijal-Resume.pdf" rel="noopener noreferrer" className={`border-solid border-2 border-indigo-600 py-2 px-4 rounded-sm ${space.className}`}>Resume</a>
+                        {/* <Link href="/about" className={`border-solid border-2 border-indigo-600 py-2 px-4 rounded-sm ${space.className}`}>Resume</Link> */}
                     </div>
                 </div>
             </div>
