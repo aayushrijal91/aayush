1220a248566efaf252eb43554b4ec9a9683d7ec2
Aayush Rijal <aayush.rijal99@gmail.com>
2023-10-03 22:49:26 +1100

    WIP pdf file

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,8 +18,8 @@ function Header() {
                         <Image src={logo.src} alt="logo" width={logo.width} height={logo.height} />
                     </div>
                     <div className='w-fit'>
-                        <a href="../files/Aayush-Rijal-Resume.pdf" rel="noopener noreferrer" className={`border-solid border-2 border-indigo-600 py-2 px-4 rounded-sm ${space.className}`}>Resume</a>
-                        {/* <Link href="/about" className={`border-solid border-2 border-indigo-600 py-2 px-4 rounded-sm ${space.className}`}>Resume</Link> */}
+                        {/* <a href="../files/Aayush-Rijal-Resume.pdf" rel="noopener noreferrer" className={`border-solid border-2 border-indigo-600 py-2 px-4 rounded-sm ${space.className}`}>Resume</a> */}
+                        <Link href="/about" className={`border-solid border-2 border-indigo-600 py-2 px-4 rounded-sm ${space.className}`}>Resume</Link>
                     </div>
                 </div>
             </div>
