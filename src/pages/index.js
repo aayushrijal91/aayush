430f7ec4adb64644d1f2c55ccf409b5b46a5de50
Aayush Rijal <arijal@aiims.com.au>
2024-02-05 08:58:16 +1100

    Arrow added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import { Space_Mono } from 'next/font/google'
 import Image from 'next/image';
+import Link from 'next/link';
 import aiims from '../images/logo/aiims.jpg';
 import yipl from '../images/logo/yipl.png';
 import reactjs from '../images/logo/reactjs.png';
@@ -54,7 +55,15 @@ function Home() {
         </div>
       </section>
 
-      <section className='py-28'>
+      <div className='-mt-20 flex justify-center'>
+        <Link href="/#experience">
+          <svg className="animate-bounce text-sky-950 bi bi-arrow-down-short" xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 16 16">
+            <path fillRule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5A.5.5 0 0 1 8 4" />
+          </svg>
+        </Link>
+      </div>
+
+      <section className='py-28' id="experience">
         <div className='container'>
           <p className='text-2xl font-bold'>Experiences</p>
 
